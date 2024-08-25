98aa730a1b0a4fadd68f14003f519056aaca0ffd
AshokAshok24 <ashok246445@gmail.com>
2024-08-25 21:01:44 +0530

    index auth api

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
-const express = require('express')
+const express = require('express');
+const jwt = require('jsonwebtoken');
 const app = express()
 
 app.use(express.json())
 
-require('dotenv').config;
+require('dotenv').config();
 
 
 
@@ -22,12 +23,33 @@ app.post('/auth', async (req, res) => {
 
     console.log("req.body", req.body);
 
-    if (typeof username !== "undefined" && username !== "" && typeof username !== null) { var userName = username } else { return res.status(200).json({ status: 0, mesage: "userName Required" }) }
+    if (typeof username !== "undefined" && username !== "" && typeof username !== null) { var username = username } else { return res.status(200).json({ status: 0, mesage: "userName Required" }) }
 
     if (typeof password !== "undefined" && password !== "" && typeof password !== null) { var password = password } else { return res.status(200).json({ status: 0, mesage: "Password Required" }) }
 
 
+    if (username !== "test@gmail.com" && password !== "test") {
 
+        return res.status(200).json({ status: 0, message: "Invalid UserName And Password" });
+
+    } else if (username == "test@gmail.com" && password !== "test") {
+
+        return res.status(200).json({ status: 0, message: "Invalid Password" });
+
+    } else if (username !== "test@gmail.com" && password == "test") {
+
+        return res.status(200).json({ status: 0, message: "Invalid UserName" });
+
+    } else if (username == "test@gmail.com" && password == "test") {
+
+
+        const token = jwt.sign({ id: 1, name: "Star", username: username }, process.env.SECRET, { expiresIn: '1m' });
+
+        console.log("token", token)
+
+        return res.status(200).json({ status: 1, message: "Valid Credentials Is Success" });
+
+    }
 
 })
 
