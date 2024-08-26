de8991206a4d7ff1a7edf6aef279a31573502c07
AshokAshok24 <ashok246445@gmail.com>
2024-08-26 21:41:17 +0530

    Home Page logout button

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,22 +1,24 @@
-    import React from 'react'
-    import { useAuth } from './utils/AuthProvider'
+import React from 'react'
+import { useAuth } from './utils/AuthProvider'
 
-    const Home = () => {
+const Home = () => {
 
-        const auth = useAuth()
+    const auth = useAuth()
 
-        const handleLogout = () => {
-            auth.logOut()
+    const handleLogout = () => {
+        auth.logOut()
 
-        }
-        return (
-            <>
-                <div>Home</div>
-
-                <button onClick={handleLogout}>Logout</button>
-
-            </>
-        )
     }
+    return (
+        <>
+            <div className='container m-5'>
 
-    export default Home
+                <div className='mb-3'>Home</div>
+
+                <button className='btn btn-primary' onClick={handleLogout}>Logout</button>
+            </div>
+        </>
+    )
+}
+
+export default Home
