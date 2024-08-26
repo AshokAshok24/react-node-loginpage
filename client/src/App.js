28503c4ff835d24bdf85f28c2ca110e1e5b4757e
AshokAshok24 <ashok246445@gmail.com>
2024-08-26 16:18:02 +0530

    client login page fucn

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,39 @@
+import Home from './components/Home';
 import './App.css';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Loginpage from './components/Loginpage';
+import ProductedRoute from './components/ProductedRoute';
+import AuthProvider from './components/utils/AuthProvider';
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <>
+
+      <div className='App'>
+
+        <Router>
+          <AuthProvider>
+
+            <Routes>
+
+              <Route path='/login' element={<Loginpage />} />
+
+              <Route element={<ProductedRoute />}>
+
+                <Route path='/home' element={<Home />} />
+
+              </Route>
+
+            </Routes>
+
+          </AuthProvider>
+        </Router>
+
+
+      </div>
+    </>
+
   );
 }
 
